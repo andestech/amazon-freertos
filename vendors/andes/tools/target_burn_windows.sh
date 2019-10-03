65541628f8e1877289e8d262c01cc9e206185766
Kevin Wang <yunkai@andestech.com>
2019-10-03 16:52:11 +0800

    Fix the bugs of burner scripts on windows.

--- vendors/andes/tools/target_burn_windows.sh
+++ vendors/andes/tools/target_burn_windows.sh
@@ -1,1 +1,1 @@
-./window_burner_tools/target_burn_frontend --erase-all --verify -P 4444 --image=$1 --algorithm-bin=./window_burner_window/target_SPI_v5_32.bin
+./windows_burner_tools/target_burn_frontend --erase-all --verify -P 4444 --image=$1 --algorithm-bin=./target_bin/target_SPI_v5_32.bin
